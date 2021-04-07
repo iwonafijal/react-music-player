28976123763e612b01d167573aeb157878345159
iwonafijal <fijal.iwona@gmail.com>
2021-04-07 14:28:01 +0200

    first commit

---
+++ src/components/LibrarySong.js
@@ -0,0 +1,48 @@
+const LibrarySong = ({
+  song,
+  songs,
+  setCurrentSong,
+  id,
+  audioRef,
+  isPlaying,
+  setSongs,
+}) => {
+  const songSelectHandler = async () => {
+    // const selectedSong = songs.filter((state) => state.id === id);
+    // setCurrentSong(selectedSong[0]);
+    // [0] bc filter returns in state array of obj, we want just an obj
+    await setCurrentSong(song);
+    // Add active state to update colour
+    const newSongs = songs.map((song) => {
+      if (song.id === id) {
+        return {
+          ...song,
+          active: true,
+        };
+      } else {
+        return {
+          ...song,
+          active: false,
+        };
+      }
+    });
+    setSongs(newSongs);
+    // check if the song is playing
+    if (isPlaying) audioRef.current.play();
+  };
+
+  return (
+    <div
+      onClick={songSelectHandler}
+      className={`library-song ${song.active ? "selected" : ""}`}
+    >
+      <img alt={song.name} src={song.cover}></img>
+      <div className="song-description">
+        <h3>{song.name}</h3>
+        <h4>{song.artist}</h4>
+      </div>
+    </div>
+  );
+};
+
+export default LibrarySong;
---
+++ src/data.js
@@ -0,0 +1,78 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillHop() {
+  return [
+    {
+      name: "Lax Incense",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/01/6b1bb8736ee3e972747bc11f312e31cf7f5823e4-1024x1024.jpg",
+      artist: "Mama Aiuto, Daphné",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=12125",
+      color: ["#15282E", "#92522F"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Small Town Palm Trees",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/01/6b1bb8736ee3e972747bc11f312e31cf7f5823e4-1024x1024.jpg",
+      artist: "Mama Aiuto, Daphné",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=12124",
+      color: ["#15282E", "#92522F"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Mirage",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/09fb436604242df99f84b9f359acb046e40d2e9e-1024x1024.jpg",
+      artist: "Nymano, j'san",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10136",
+      color: ["#5A5482", "#946A9A"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Beaver Creek",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10076",
+      color: ["#17312E", "#1A5E5D"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Paved Paths",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/88e7eb711f8c71d87fc102e97cf91e36f692348d-1024x1024.jpg",
+      artist: "Leavy",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9919",
+      color: ["#D4D4A4", "#D39364"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Colors Fade",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/c209a7df7b9bc133dfff73ce86ebc3c57c2b73dd-1024x1024.jpg",
+      artist: "Sleepy Fish",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10023",
+      color: ["#366684", "#C9E3E5"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Paved Paths",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/88e7eb711f8c71d87fc102e97cf91e36f692348d-1024x1024.jpg",
+      artist: "Leavy",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9919",
+      color: ["#D4D4A4", "#D39364"],
+      id: uuidv4(),
+      active: false,
+    },
+  ];
+}
+
+export default chillHop;
